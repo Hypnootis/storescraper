e127e059b8212689048309cad866edddc53f926e
Nikita <nikita1231515@gmail.com>
2023-11-14 19:08:41 +0200

    added basic frontend functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,58 @@
 import React, { useState, useEffect } from "react";
+import Item from "./Item";
+import Spinner from "./Spinner";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [result, setResult] = useState([]);
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
-    if (clicked)
-      fetch("http://127.0.0.1:8080/api/search")
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-
-    return function () {
-      setClicked(false);
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `http://127.0.0.1:8080/api/search/${query}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setResult(Object.values(data));
+      } catch (error) {
+        console.error("Fetch error:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
+
+    if (clicked) {
+      fetchData();
+      setClicked(false);
+    }
   }, [clicked]);
 
   return (
-    <div>
+    <div className="flex-container">
       <header>
-        <p>{message || "Loading..."}</p>
+        <p>Data Mining App</p>
       </header>
-      <button onClick={() => setClicked(!clicked)}>Bruh</button>
+      <button onClick={() => setClicked(true)}>Search</button>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search producs..."
+      />
+      {!isLoading ? (
+        <ul>
+          {result?.map((item) => (
+            <Item product={item} key={item.product_name} />
+          ))}
+        </ul>
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 }
--- server/app.py
+++ server/app.py
@@ -2,6 +2,34 @@ from selenium_scrape import get_searches
 from flask_cors import CORS
 from flask import Flask, jsonify
 
+# test_data = {
+#     "0": {
+#         "comparison_price": "0,89 \u20ac/l",
+#         "product_name": "Kotimaista kevytmaito 1 L",
+#         "unit_price": "0,89 \u20ac",
+#     },
+#     "1": {
+#         "comparison_price": "1,25 \u20ac/l",
+#         "product_name": "Kotimaista laktoositon kevytmaitojuoma 1 L",
+#         "unit_price": "1,25 \u20ac",
+#     },
+#     "2": {
+#         "comparison_price": "0,79 \u20ac/l",
+#         "product_name": "Kotimaista rasvaton maito 1l",
+#         "unit_price": "0,79 \u20ac",
+#     },
+#     "3": {
+#         "comparison_price": "1,19 \u20ac/l",
+#         "product_name": "Kotimaista t\u00e4ysmaito 1l",
+#         "unit_price": "1,19 \u20ac",
+#     },
+#     "4": {
+#         "comparison_price": "3,16 \u20ac/kg",
+#         "product_name": "Kotimaista 250g rasvaton laktoositon maitorahka",
+#         "unit_price": "0,79 \u20ac",
+#     },
+# }
+
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
@@ -11,9 +39,9 @@ def hello_world():
     return jsonify(message="Hello, World!")
 
 
-@app.route("/api/search")
-def search():
-    return jsonify(get_searches("maito", False))
+@app.route("/api/search/<param>")
+def search(param):
+    return jsonify(get_searches(param, False))
 
 
 if __name__ == "__main__":
