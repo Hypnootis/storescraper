3cf767788a08522abdce5686dce34f07856cc6d0
Tuomas Pasanen <tuomas.pasanen@edu.lapinamk.fi>
2023-11-14 11:24:29 +0200

    Can now query and output to JSON file

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -3,23 +3,55 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import json
+
+sort_params = {"lowest_comparison_price": "&sort=comparisonPrice%3Aasc",
+                     "highest_comparison_price": "&sort=comparisonPrice%3Adesc" }
 
 # Init driver
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
-chrome_options.headless = True
-driver = webdriver.Chrome(options=chrome_options)
+def init_driver() -> webdriver:
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
 
-def get_searches(query: str, driver: webdriver):
-   driver.get("https://www.s-kaupat.fi/hakutulokset?queryString=" + query)
-   element = WebDriverWait(driver=driver, timeout=5).until(
-       EC.visibility_of_element_located((By.CSS_SELECTOR, "article[data-test-id=product-card]"))
+def get_searches(query: str, output_json: bool, sorting_parameters: str):
+    """Query the S-Kaupat database
+
+    query: str, the query string
+
+    output_json: bool, if False, prints query results, if True, outputs to queryResults.json
+
+    sorting_parameters: "lowest_comparison_price", "highest_comparison_price" or leave blank for no sorting
+    """
+    driver = init_driver()
+    url = "https://www.s-kaupat.fi/hakutulokset?queryString="
+    if sorting_parameters != "":
+        driver.get(url + query + sort_params[sorting_parameters])
+    else:
+        driver.get(url + query)
+    element = WebDriverWait(driver=driver, timeout=5).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "article[data-test-id=product-card]"))
    )
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    if output_json:
+       query_to_json(soup, False)
+    else:
+        query_to_json(soup, True)
+    driver.quit()
 
-get_searches("kana", driver)
+def query_to_json(soup: BeautifulSoup, only_print: bool):
+    products = {}
+    for index, tag in enumerate(soup.select("article[data-test-id=product-card]", limit=5)):
+        if only_print:
+            print("Product name: " + tag.select_one("a[data-test-id=product-card__productName]").text)
+            print("Product price: " + tag.select_one("span[data-test-id=product-price__unitPrice]").text)
+            print("Product comparison price: " + tag.select_one("div[data-test-id=product-card__productPrice__comparisonPrice]").text)
+        else:
+            products.update({index: {}})
+            products[index].update({"product_name": tag.select_one("a[data-test-id=product-card__productName]").text})
+            products[index].update({"Unit price": tag.select_one("span[data-test-id=product-price__unitPrice]").text})
+            products[index].update({"Comparison price": tag.select_one("div[data-test-id=product-card__productPrice__comparisonPrice]").text})
 
-soup = BeautifulSoup(driver.page_source, "html.parser")
-beautiful_page = soup.prettify()
-with open("selenium_cheeses.html", "w") as file:
-    file.write(beautiful_page)
-driver.quit()
+            with open("queryResults.json", "w") as file:
+                json.dump(products, file, indent=4)
---
+++ test.py
@@ -0,0 +1,3 @@
+from selenium_scrape import get_searches
+
+get_searches("juusto", False, "highest_comparison_price")
